4701c965ab4e25ed380007912860b1843237867f
baltoky <cesarsantiago316@gmail.com>
2019-03-31 17:53:15 -0500

    organizing stuff

--- COP4710-SecondRun/Spools/Q62.sql
+++
@@ -1,9 +0,0 @@
-SELECT s.sname, s.class, t.course_num
-FROM Student s, Section t, Grade g
-WHERE s.stno = g.student_number
-AND g.section_Id = t.section_Id
-MINUS
-SELECT s.sname, s.class, t.course_num
-FROM Student s, Section t, Grade g
-WHERE s.class = 2
-/
--- COP4710-SecondRun/Spools/Q65a.sql
+++
@@ -1,7 +0,0 @@
-SELECT s.stno, s.sname, t.course_num, g.grade
-FROM Student s, Section t, Grade g
-WHERE s.stno = g.student_number
-AND g.section_Id = t.section_Id
-AND t.course_num LIKE 'COSC%'
-AND (g.grade = 'A' OR g.grade = 'B')
-/
