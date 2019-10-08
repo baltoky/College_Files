5079a880ef4b8a8cee96832303e4f1941d6bb1a7
baltoky <cesarsantiago316@gmail.com>
2019-10-07 20:45:19 -0500

    nodejs stuff

---
+++ CEN4053/hotTamales/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const fs = require("fs");
+
+let logStream = fs.createWriteStream("./log.txt", {flags: "a"});
+let app = express();
+ 
+//use something for logging
+ 
+// may need fs filestream
+app.set("port", 80);
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {stream: logStream}));
+ 
+app.get("/version", function(req, res) {
+  res.send("This is hotBurger version 0.1");
+});
+app.get("/logs", function(req, res) {
+	let r = fs.readFileSync("./log.txt", "UTF8");
+  res.send(r);
+});
+ 
+ 
+app.listen(app.get("port"), function() {
+  console.log(
+    "Express started on http://localhost/:" +
+      app.get("port") +
+      "; press Ctrl-C to terminate."
+  );
+});
